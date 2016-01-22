2ce2faa8b3247a8e9aba45fdf07a3d5f8c8a0985
pradeep kumar B <b.pradeep@samsung.com>
2016-01-22 10:17:27 +0530

    [capi-http] Added Http module APIs 1. Added HttpSession, HttpTransaction, HttpHeader, HttpRequest, HttpResponse APIs 2. Added HttpSession, HttpTransaction data structures
    Change-Id: Ic30fe078b26bd4845acf2df857fd048ec0a5af1e
    Signed-off-by: pradeep kumar B <b.pradeep@samsung.com>

--- include/http_private.h
+++ include/http_private.h
@@ -17,11 +17,127 @@
 #ifndef __HTTP_PRIVATE_H__
 #define __HTTP_PRIVATE_H__
 
+#define LOG_TAG	"CAPI_NETWORK_HTTP"
+
+#include <string.h>
+#include <glib.h>
+#include <gio/gio.h>
+#include <curl/curl.h>
+#include <dlog.h>
+
+#include "http.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+#ifndef API
+#define API __attribute__ ((visibility("default")))
+#endif
+
+#ifndef DEPRECATED_API
+#define DEPRECATED_API __attribute__ ((deprecated))
+#endif
+
+#define DBG(fmt, args...)	LOGD(fmt, ##args)
+#define WARN(fmt, args...)	LOGW(fmt, ##args)
+#define ERR(fmt, args...)	LOGE(fmt, ##args)
+#define SDBG(fmt, args...)	SECURE_LOGD(fmt, ##args)
+#define SERR(fmt, args...)	SECURE_LOGE(fmt, ##args)
+
+#define _warn_if(expr, fmt, arg...) do { \
+		if (expr) { \
+			WARN(fmt, ##arg); \
+		} \
+	} while (0)
+
+#define _ret_if(expr) do { \
+		if (expr) { \
+			return; \
+		} \
+	} while (0)
+
+#define _retv_if(expr, val) do { \
+		if (expr) { \
+			return (val); \
+		} \
+	} while (0)
+
+#define _retm_if(expr, fmt, arg...) do { \
+		if (expr) { \
+			ERR(fmt, ##arg); \
+			return; \
+		} \
+	} while (0)
+
+#define _retvm_if(expr, val, fmt, arg...) do { \
+		if (expr) { \
+			ERR(fmt, ##arg); \
+			return (val); \
+		} \
+	} while (0)
+
+
+typedef struct {
+	struct curl_slist *header_list;
+} __http_header_h;
+
+typedef struct {
+	gchar *proxy_addr;
+	gchar *host_uri;
+	gchar *method;
+	gchar *encoding;
+	http_version_e http_version;
+	gchar* body;
+} __http_request_h;
+
+typedef struct {
+	http_status_code_e status_code;
+	gchar	*status_text;
+} __http_response_h;
+
+typedef struct {
+	CURLM *multi_handle;
+	guint timer_event;
+	int still_running;
+	int active_transaction_count;
+	gboolean auto_redirect;
+	http_session_mode_e session_mode;
+} __http_session_h;
+
+typedef struct {
+	CURL *easy_handle;
+	gchar *interface_name;
+	int timeout;
+	gchar error[CURL_ERROR_SIZE];
+
+	int socket_fd;
+	/*Transaction Callbacks */
+	http_transaction_header_cb header_cb;
+	http_transaction_body_cb body_cb;
+	http_transaction_write_cb write_cb;
+	http_transaction_completed_cb completed_cb;
+	http_transaction_aborted_cb aborted_cb;
+	/*Progress Callbacks */
+	http_transaction_upload_progress_cb upload_progress_cb;
+	http_transaction_download_progress_cb download_progress_cb;
+
+	__http_session_h *session;
+	__http_request_h *request;
+	__http_response_h *response;
+	__http_header_h *header;
+
+	GThread *thread;
+	GMainLoop *thread_loop;
+} __http_transaction_h;
+
+typedef struct {
+	GIOChannel* channel;
+	GSource* source;
+	int action;
+
+	__http_session_h *session;
+} __http_socket_info_h;
 
 #ifdef __cplusplus
  }
