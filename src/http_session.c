6f0edb96693f857dcd68e0d0de334bd72a6c8142
pradeep kumar B <b.pradeep@samsung.com>
2016-01-27 15:39:26 +0530

    [capi-http] Added basic http method implementation 1.support multi session multi transaction
    Change-Id: If247c783cc357f2d4957572979bbcc99d7a0db63
    Signed-off-by: pradeep kumar B <b.pradeep@samsung.com>

--- src/http_session.c
+++ src/http_session.c
@@ -17,3 +17,309 @@
 #include "http.h"
 #include "http_private.h"
 
+void _check_curl_multi_status(gpointer user_data)
+{
+	__http_transaction_h *transaction = NULL;
+	__http_session_h *session = (__http_session_h *)user_data;
+
+	CURLMsg* message = NULL;
+	int count = 0;
+	CURL* curl_easy = NULL;
+	char* url = NULL;
+	CURLcode curl_code = CURLE_OK;
+
+	message = curl_multi_info_read(session->multi_handle, &count);
+
+	while (message != NULL) {
+		if (message->msg == CURLMSG_DONE) {
+			curl_easy = message->easy_handle;
+			curl_code = message->data.result;
+			curl_easy_getinfo(curl_easy, CURLINFO_PRIVATE, &transaction);
+			curl_easy_getinfo(curl_easy, CURLINFO_EFFECTIVE_URL, &url);
+
+			DBG("Completed -%s: result(%d)\n", url, curl_code);
+
+			if (curl_code == CURLE_OK) {
+				transaction->completed_cb();
+			} else {
+
+			}
+
+			curl_multi_remove_handle(session->multi_handle, curl_easy);
+			g_main_loop_quit((GMainLoop*)transaction->thread_loop);
+		}
+		message = curl_multi_info_read(session->multi_handle, &count);
+	}
+}
+
+gboolean timer_expired_callback(gpointer user_data)
+{
+	__http_session_h* session = (__http_session_h *)user_data;
+
+	CURLMcode ret;
+
+	ret = curl_multi_socket_action(session->multi_handle, CURL_SOCKET_TIMEOUT, 0, &(session->still_running));
+	if (ret == CURLM_OK) {
+		//DBG("CURLM_OK - Called curl_multi_socket_action()\n");
+	} else {
+		print_curl_multi_errorCode(ret);
+	}
+
+	_check_curl_multi_status(session);
+
+	return FALSE;
+}
+
+gboolean _handle_event(int fd, int action, gpointer user_data)
+{
+	__http_session_h *session = (__http_session_h *)user_data;
+
+	int running_handles = -1;
+
+	CURLMcode ret = CURLM_OK;
+
+	ret = curl_multi_socket_action(session->multi_handle, fd, action, &running_handles);
+	if (ret == CURLM_OK) {
+		//DBG("CURLM_OK: Called curl_multi_socket_action(%d)\n", action);
+	}
+	else {
+		print_curl_multi_errorCode(ret);
+	}
+
+	_check_curl_multi_status(session);
+
+	if (running_handles > 0) {
+		return TRUE;
+	} else {
+		DBG("last transfer done, kill timeout\n");
+		if (session->timer_event) {
+			g_source_remove(session->timer_event);
+			session->timer_event = 0;
+		}
+		return FALSE;
+	}
+}
+
+gboolean __handle_socket_received_event_cb(GIOChannel *channel, GIOCondition condition, gpointer user_data)
+{
+	int fd, action, ret;
+
+	if (condition & (G_IO_NVAL | G_IO_HUP | G_IO_ERR))
+		return FALSE;
+
+	fd = g_io_channel_unix_get_fd(channel);
+
+	//CURL_CSELECT_IN : 1, CURL_CSELECT_OUT: 2
+	action = (condition & G_IO_IN ? CURL_CSELECT_IN : 0) | (condition & G_IO_OUT ? CURL_CSELECT_OUT : 0);
+
+	ret = _handle_event(fd, action, user_data);
+	if (ret) {
+		return TRUE;
+	}
+
+	return FALSE;
+}
+
+/* Clean up the __http_socket_info_h structure */
+static void _remove_socket_info(__http_socket_info_h *sock_info)
+{
+	if (!sock_info) {
+		return;
+	}
+	if (sock_info->event) {
+		g_source_remove(sock_info->event);
+		sock_info->event = 0;
+	}
+	if (sock_info->channel) {
+		g_io_channel_unref(sock_info->channel);
+		sock_info->channel = NULL;
+	}
+	g_free(sock_info);
+	sock_info = NULL;
+}
+
+/* Assign socket information to a __http_socket_info_h structure */
+static void _set_socket_info(__http_socket_info_h *sock_info, curl_socket_t fd, CURL *curl_easy, int action, void *user_data)
+{
+	__http_session_h *session = (__http_session_h *)user_data;
+	GIOCondition condition = (action & CURL_POLL_IN ? G_IO_IN : 0) | (action & CURL_POLL_OUT ? G_IO_OUT: 0);
+
+	sock_info->sockfd = fd;
+	sock_info->action = action;
+	sock_info->easy_handle = curl_easy;
+	if (sock_info->event) {
+		g_source_remove(sock_info->event);
+		sock_info->event = 0;
+	}
+	sock_info->event = g_io_add_watch(sock_info->channel, condition, __handle_socket_received_event_cb, session);
+}
+
+/* Initialize a new Socket Info structure */
+static void _add_socket_info(curl_socket_t fd, CURL *curl_easy, int action, void *user_data)
+{
+	__http_session_h *session = (__http_session_h *)user_data;
+	__http_socket_info_h *sock_info = (__http_socket_info_h *)malloc(sizeof(__http_socket_info_h));
+
+	sock_info->session = session;
+	sock_info->channel = g_io_channel_unix_new(fd);
+	_set_socket_info(sock_info, fd, curl_easy, action, session);
+	curl_multi_assign(session->multi_handle, fd, sock_info);
+}
+
+int __handle_socket_cb(CURL *curl_easy, curl_socket_t fd, int action, void *user_data, void *socketp)
+{
+	__http_session_h *session = (__http_session_h *)user_data;
+	__http_socket_info_h *sock_info = (__http_socket_info_h*) socketp;
+
+	static const char *actionstr[]={ "none", "IN", "OUT", "INOUT", "REMOVE"};
+
+	DBG("__handle_socket_cb: fd=%d easy_handle=%p action=%s ", fd, curl_easy, actionstr[action]);
+	if (action == CURL_POLL_REMOVE) {
+		DBG("\n");
+		_remove_socket_info(sock_info);
+	} else {
+		if (!sock_info) {
+			DBG("Adding data: %s%s\n", action & CURL_POLL_IN ? "READ":"", action & CURL_POLL_OUT ? "WRITE":"" );
+			_add_socket_info(fd, curl_easy, action, session);
+		}
+		else {
+			DBG("Changing action from %d to %d\n", sock_info->action, action);
+			_set_socket_info(sock_info, fd, curl_easy, action, session);
+		}
+	}
+
+	return 0;
+}
+
+int __handle_timer_cb(CURLM *curl_multi, long timeout_ms, void *user_data)
+{
+	__http_session_h* session = (__http_session_h *)user_data;
+
+	session->timer_event = g_timeout_add(timeout_ms , timer_expired_callback , session);
+
+	return 0;
+}
+
+API int http_init()
+{
+	if (curl_global_init(CURL_GLOBAL_ALL) != CURLE_OK) {
+		DBG("curl_global_init failed, so returning!\n");
+		return HTTP_ERROR_OPERATION_FAILED;
+	}
+
+	return HTTP_ERROR_NONE;
+}
+
+API void http_deinit()
+{
+	curl_global_cleanup();
+}
+
+API int http_create_session(http_session_h *http_session, http_session_mode_e mode)
+{
+	_retvm_if(http_session == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(http_session) is NULL\n");
+
+	__http_session_h *session = NULL;
+
+	session = (__http_session_h *)malloc(sizeof(__http_session_h));
+
+	session->multi_handle = curl_multi_init();
+	session->active_transaction_count = 0;
+	session->session_mode = mode;
+	session->auto_redirect = FALSE;
+
+	curl_multi_setopt(session->multi_handle, CURLMOPT_SOCKETFUNCTION, __handle_socket_cb);
+	curl_multi_setopt(session->multi_handle, CURLMOPT_SOCKETDATA, session);
+	curl_multi_setopt(session->multi_handle, CURLMOPT_TIMERFUNCTION, __handle_timer_cb);
+	curl_multi_setopt(session->multi_handle, CURLMOPT_TIMERDATA, session);
+
+	if (mode == HTTP_SESSION_MODE_PIPELINING) {
+		curl_multi_setopt(session->multi_handle, CURLMOPT_PIPELINING, 1L);
+	}
+
+	*http_session = (http_session_h)session;
+
+	return HTTP_ERROR_NONE;
+}
+
+API int http_delete_session(http_session_h http_session)
+{
+	_retvm_if(http_session == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(http_session) is NULL\n");
+
+	__http_session_h *session = (__http_session_h *)http_session;
+
+	if (session->multi_handle) {
+		curl_multi_cleanup(session->multi_handle);
+		session->multi_handle = NULL;
+	}
+
+	session->active_transaction_count = 0;
+	session->still_running = 0;
+	session->auto_redirect = FALSE;
+
+	free(session);
+
+	return HTTP_ERROR_NONE;
+}
+
+API int http_session_set_auto_redirection(http_session_h http_session, bool enable)
+{
+	_retvm_if(http_session == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(http_session) is NULL\n");
+
+	__http_session_h *session = (__http_session_h *)http_session;
+
+	session->auto_redirect = enable;
+
+	return HTTP_ERROR_NONE;
+}
+
+API int http_session_get_auto_redirection(http_session_h http_session, bool *auto_redirect)
+{
+	_retvm_if(http_session == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(http_session) is NULL\n");
+	_retvm_if(auto_redirect == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(auto_redirect) is NULL\n");
+
+	__http_session_h *session = (__http_session_h *)http_session;
+
+	*auto_redirect =  session->auto_redirect;
+
+	return HTTP_ERROR_NONE;
+}
+
+API int http_session_get_active_transaction_count(http_session_h http_session, int *active_transaction_count)
+{
+	_retvm_if(http_session == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(http_session) is NULL\n");
+	_retvm_if(active_transaction_count == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(active_transaction_count) is NULL\n");
+
+	__http_session_h *session = (__http_session_h *)http_session;
+
+	*active_transaction_count = session->active_transaction_count;
+
+	return HTTP_ERROR_NONE;
+}
+
+API int http_session_get_max_transaction_count(http_session_h http_session, int *transaction_count)
+{
+	_retvm_if(http_session == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(http_session) is NULL\n");
+	_retvm_if(transaction_count == NULL, HTTP_ERROR_INVALID_PARAMETER,
+			"parameter(transaction_count) is NULL\n");
+
+	__http_session_h *session = (__http_session_h *)http_session;
+
+	if (session->session_mode == HTTP_SESSION_MODE_NORMAL) {
+		*transaction_count =  _MAX_HTTP_TRANSACTIONS_PER_SESSION_NORMAL;
+	} else if (session->session_mode == HTTP_SESSION_MODE_PIPELINING) {
+		*transaction_count =  _MAX_HTTP_TRANSACTIONS_PER_SESSION_PIPE;
+	} else {
+		*transaction_count =  -1;
+	}
+
+	return HTTP_ERROR_NONE;
+}
